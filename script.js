55a37de0671938ccbd382f61c1b39ae45346cb0c
xFabriicioJS <fabriciocostamonteiro@hotmail.com>
2022-09-26 13:27:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const validacao = document.get('.validacao');
+const nome =  document.getElementById('nomeCompleto');
+const button = document.getElementById('teste');
+const texto = document.getElementById('texto');
+button.addEventListener('click', (e)=>{
+    e.preventDefault();
+    validacao[0].innerHTML = "teste";
+
+
+
+
+    // document.querySelectorAll('.validacao').innerHTML = "TESTES"
+    // let messages = [];
+  
+
+    // console.log(validacao);
+    // if(nome.value < 3) {
+            
+    //         messages.push("Seu nome deve conter mais de três caracteres.");
+
+    //     }
+    //     if(messages.length > 0){
+    //         validacao.forEach(val => {
+    //             val.innerHTML = 'teste'
+    //         })
+    //     }
+    
+
+    })
